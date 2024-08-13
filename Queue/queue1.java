103c5d973415b9111a4c237d3f6af7b255a466f1
Snehal Gaikwad <161202223+SnehalGaikwad-04@users.noreply.github.com>
2024-08-13 16:35:40 +0530

    Update queue1.java

--- Queue/queue1.java
+++ Queue/queue1.java
@@ -1,5 +1,6 @@
 //creating a queue using Array
 //not recommended
+
 package Queue;
 
 public class Queue1 {
@@ -17,8 +18,8 @@ public class Queue1 {
 		public static boolean isEmpty() {
 			return rear == -1;
 		}
-		
-		//enqueue
+
+		// enqueue
 		public static void add(int data) {
 			if (rear == size - 1) {
 				System.out.println("the queue is full");
@@ -27,42 +28,46 @@ public class Queue1 {
 			rear++;
 			arr[rear] = data;
 		}
-		
-		//dequeue
+
+		// dequeue
 		public static int remove() {
-			if(isEmpty()) {
+			if (isEmpty()) {
 				System.out.println("queue empty");
 				return -1;
 			}
 			int front = arr[0];
-			for(int i=0; i<rear; i++) {
-				arr[i] = arr[i+1];
+			for (int i = 0; i < rear; i++) {
+				arr[i] = arr[i + 1];
 			}
 			rear--;
 			return front;
 		}
-		
-		//peek
+
+		// peek
 		public static int peek() {
-			if(isEmpty()) {
+			if (isEmpty()) {
 				System.out.println("queue empty");
 				return -1;
 			}
 			return arr[0];
-		}		
+		}
+
 	}
-	
+
 	public static void main(String[] args) {
 		Queue q = new Queue(5);
-		
+
 		q.add(1);
 		q.add(2);
 		q.add(3);
 		q.add(4);
-		
+
 		while (!q.isEmpty()) {
 			System.out.println(q.peek());
-			q.remove();			
+			q.remove();
+
 		}
+
 	}
+
 }
